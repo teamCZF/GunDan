ace2219011851b79268f33277bbc0a8fc955b063
yuanyuanfan <1095792032@qq.com>
2016-12-25 21:31:58 +0800

    setInfoAct

--- Gundan/src/main/java/com/ui/SetInfoActivity.java
+++ Gundan/src/main/java/com/ui/SetInfoActivity.java
@@ -2,6 +2,8 @@ package com.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.base.basepedo.R;
 
@@ -9,10 +11,31 @@ import com.base.basepedo.R;
  * Created by Administrator on 2016/12/19 0019.
  */
 
-public class SetInfoActivity extends Activity {
+public class SetInfoActivity extends Activity implements View.OnClickListener{
+    private Button backToUser;
+    private Button saveInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setinfo);
+        backToUser =(Button)findViewById(R.id.backToUser);
+        backToUser.setOnClickListener(this);
+        saveInfo =(Button)findViewById(R.id.save_info);
+        saveInfo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.backToUser:
+                finish();
+                break;
+            case R.id.save_info:
+                break;
+            default:
+                break;
+        }
+
     }
 }
